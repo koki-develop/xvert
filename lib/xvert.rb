af56d9f9c5a0ae4c6085db24364b7eee21e9a623
koki-develop <kou.pg.0131@gmail.com>
2023-05-07 09:54:00 +0900

    bundle gem xvert

---
+++ lib/xvert.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "xvert/version"
+
+module Xvert
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,6 @@
+# frozen_string_literal: true
+
+$LOAD_PATH.unshift File.expand_path("../lib", __dir__)
+require "xvert"
+
+require "minitest/autorun"
