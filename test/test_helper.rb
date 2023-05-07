3f64911f3a3d0d83bd1867a0d2fd67ab3c33274c
koki-develop <kou.pg.0131@gmail.com>
2023-05-07 16:17:47 +0900

    add simplecov

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,5 +1,11 @@
 # frozen_string_literal: true
 
+require "simplecov"
+SimpleCov.start
+
+require "simplecov-cobertura"
+SimpleCov.formatter = SimpleCov::Formatter::CoberturaFormatter
+
 $LOAD_PATH.unshift File.expand_path("../lib", __dir__)
 require "xvert"
 
