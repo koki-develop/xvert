a07d6b50aae0a35378c43101bd90a4ccf1d5a9c3
koki-develop <kou.pg.0131@gmail.com>
2023-05-07 11:35:20 +0900

    implement jy command

--- lib/xvert.rb
+++ lib/xvert.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
+require "json"
+require "yaml"
 require_relative "xvert/version"
 
 module Xvert
-  class << self
-    def hello
-      puts "Hello World"
-    end
+  def self.json_to_yaml(text)
+    JSON.parse(text).to_yaml
   end
 end
